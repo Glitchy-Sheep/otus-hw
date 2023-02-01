fecb8d6032a81d115c717cb37572b269ade5bd78
Glitchy-Sheep <crazzzy_coder@mail.ru>
2023-02-01 10:34:24 +0300

    add ctest and some checks for test data
    Sometimes I forget to generate results.txt or place output.txt
    in test_data directory, and also these files may not be opened.
    
    So the check for file_stream.fail() is required.
    
    [1. ip filter]

--- 1. ip filter/src/test.cpp
+++ 1. ip filter/src/test.cpp
@@ -3,24 +3,30 @@
 
 int main()
 {
-    // Data must be generated in results.txt before start
+    // Data must be generated in results.txt (by the main program) before start
+    // via  "data | main.exe > result.txt" command
     // Test is successful if there is no difference in files
-    std::ifstream input{"results.txt", std::ios_base::in};
+    std::ifstream results{"results.txt", std::ios_base::in};
     std::ifstream output{"output.txt", std::ios_base::in};
 
+    if (results.fail())
+        throw std::runtime_error("There is no results.txt data file.");
+    if (output.fail())
+        throw std::runtime_error("There is no output.txt data file.");
+
     int line_n = 1;
-    while(!input.eof() && !output.eof())
+    while(!results.eof() && !output.eof())
     {
         std::string line1;
         std::string line2;
 
-        input  >> line1;
+        results >> line1;
         output >> line2;
 
         if (line1 != line2)
         {
             std::cout << "Difference in the [" << line_n++ << "] line\n";
-            std::cout << "input:\t[" << line1 << "]\n";
+            std::cout << "results:\t[" << line1 << "]\n";
             std::cout << "output:\t[" << line2 << ']' << std::endl;
         }
     }
